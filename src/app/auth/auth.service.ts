76383ae6914b92754025aa40906a727efca9a2cb
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-11 08:46:42 +0100

    update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LoginPayLoad } from './login/login-pay-load';
 import { RegisterPayLoad } from './register/register-pay-load';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  url="http://localhost:8080/api/auth";
+  url="http://localhost:8080/api/auth/";
 
   constructor(private http : HttpClient) {
    }
    registerd(register:RegisterPayLoad):Observable<any> {
-    
-    return this.http.post(this.url+"/signup",register);
+    return this.http.post(this.url+"signup",register);
+   }
 
+   login(register:LoginPayLoad):any{
+      return this.http.post(this.url+"login",register);
    }
 }
