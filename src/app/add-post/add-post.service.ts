1e58e9defc1de614b3ded40d0fc357a9eb320f8a
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-12 23:35:19 +0100

    create addPost method

--- src/app/add-post/add-post.service.ts
+++ src/app/add-post/add-post.service.ts
@@ -1,14 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddPostPayLoad } from './add-post-pay-load';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddPostService {
 
+  url="http://localhost:8080/api/posts/";
+
   constructor(private http:HttpClient) {
 
   }
-  
+  addPost(postPayLoad:AddPostPayLoad){
+    this.http.post(this.url,postPayLoad);
+  }
 
 }
