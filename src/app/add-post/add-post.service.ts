e7669e272213af8d5d6838d56080a337a73f8c68
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-12 23:31:49 +0100

    inject httpClient

--- src/app/add-post/add-post.service.ts
+++ src/app/add-post/add-post.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,6 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class AddPostService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+
+  }
   
+
 }
