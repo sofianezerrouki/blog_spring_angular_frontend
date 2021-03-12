7dfe3fa8932ca4f04053ad856d22c1ec4311ed82
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-12 23:17:32 +0100

    add add-post-pay-load

---
+++ src/app/add-post/add-post-pay-load.spec.ts
@@ -0,0 +1,7 @@
+import { AddPostPayLoad } from './add-post-pay-load';
+
+describe('AddPostPayLoad', () => {
+  it('should create an instance', () => {
+    expect(new AddPostPayLoad()).toBeTruthy();
+  });
+});
---
+++ src/app/add-post/add-post-pay-load.ts
@@ -0,0 +1,6 @@
+export class AddPostPayLoad {
+    id :number;
+    title:string;
+    content:string;
+    username:string;
+}
